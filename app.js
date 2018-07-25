a8651ef983184f703da4eb1554cf45bb0ddd46e5
Phuong Tran <trantuanphuong19941994@gmail.com>
2018-07-25 22:34:28 +0700

    Date

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const person = {
-  firstName: 'Tran',
-  lastName: 'Phuong',
-  age: 24,
-  email: 'trantuanphuong19941994@gmail.com',
-  hobbies: ['music', 'code'],
-  address: {
-    city: 'New York',
-    state: 'Florida'
-  },
-  getBirthYear: function() {
-    return 2018 - this.age;
-  }
-};
+// Date time
 
-console.log(person['firstName']);
-console.log(person['age']);
-console.log(person['hobbies']);
-console.log(person['address']['city']);
-console.log(person.getBirthYear());
+let val;
+const today = new Date();
+let birthday = new Date('December 3 1994');
+val = today;
 
-const people = [
-  {name: 'John', age: 30},
-  {name: 'Mike', age: 27}
-];
-
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i]);
-}
+console.log(birthday.getDate());
