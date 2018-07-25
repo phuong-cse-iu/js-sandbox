a1d3524e34560bd3ad93ec941b9e078f27643616
Phuong Tran <trantuanphuong19941994@gmail.com>
2018-07-25 21:06:01 +0700

    Math object

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-// Type conversion
 let val;
 
-// Number to string
-val = String(555);
-val = String(4 + 4);
+val = Math.floor(Math.random() * 20 + 1); // random from 1 - 20
 
-// Boolean to string
-val = String(true);
-
-// Date to string
-val = String(new Date());
-
-// Array to string
-val = String([1, 2, 3, 4]);
-
-// String to number
-val = Number('5.5');
-val = Number(true);
-val = Number(null);
-val = Number('hello');
-val = Number([1, 2, 3, 4]);
-
-val = parseInt('100.30');
-val = parseFloat('100.10');
-
-const val1 = 5;
-const val2 = String(6);
-const sum = val1 + val2;
-
-// Output
-console.log(sum);
-console.log(typeof sum);
+console.log(val);
