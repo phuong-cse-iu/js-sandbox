631acbc2d35db84c0fd5c1a0406ed8eefc9f70a5
Phuong Tran <trantuanphuong19941994@gmail.com>
2018-07-26 21:18:56 +0700

    DOM work-around

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-// Window object
+let val;
 
-let val = window.history.length;
+val = document;
 
-// Navigator Object
-val = window.navigator.appVersion;
-console.log(val);
+val = document.all; // html, head, meta, meta, meta, link, link, title, body, div.container, div.row, div.col.s12, div#main.card, div.card-content, span.card-title, div.row, form#task-form, div.input-field.col.s12, input#task, label, input.btn, div.card-action, h5#task-title, ul.collection, li.collection-item, a.delete-item.secondary-content, i.fa.fa-remove, li.collection-item, a.delete-item.secondary-content, i.fa.fa-remove, li.collection-item, a.delete-item.secondary-content, i.fa.fa-remove, li.collection-item, a.delete-item.secondary-content, i.fa.fa-remove, li.collection-item, a.delete-item.secondary-content, i.fa.fa-remove, a.clear-tasks.btn.black, script, script, script, viewport: meta, main: div#main.card, task-form: form#task-form, task: input#task, task-title: h5#task-title
+
+val = document.doctype;
+val = document.body;
+val = document.head;
+val = document.contentType;
+val = document.forms;
+val = document.characterSet; // UTF-8
+val = document.forms[0].method; // GET
+val = document.forms[0].action;
+
+val = document.links[0].classList;
+
+console.log(val); 
+
+
