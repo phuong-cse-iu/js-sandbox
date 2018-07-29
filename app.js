91ad63cdd4a991ab563e14671c31b18bdea32001
Phuong Tran <trantuanphuong19941994@gmail.com>
2018-07-29 17:09:50 +0700

    Add event listener

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-// REPLACE ELEMENT
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-// Create element
-const newHeading = document.createElement('h2');
-newHeading.id = 'task-title';
-newHeading.appendChild(document.createTextNode('Task List'));
+function onClick(ev) {
 
-// Get old heading 
-const oldHeading = document.getElementById('task-title');
-// Parent
-const cardAction = document.querySelector('.card-action');
-
-// Relace
-cardAction.replaceChild(newHeading, oldHeading);
+  let val;
+  val = ev;
+  val.target.textContent = 'Hello';
 
 
-// REMOVE ELEMENT
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+  // Event type
+  val = ev.type;
 
-// Remove list item
-lis[0].remove();
+  // Coords event relative to the window
+  val = ev.clientY;
 
-// Remove child element
-list.removeChild(lis[4]);
-
-// CLASSES & ATTR
-
-// Classes
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
-let val;
-val = link.className;
-val = link.classList;
-val = link.classList.add('test');
-val = link.classList.remove('test');
-val = link;
-
-// Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'https://google.com');
-link.setAttribute('title', 'google');
-link.removeAttribute('title');
-val = link.hasAttribute('title');
-
-
-console.log(val);
+  // Coords event relative to the element
+  val = ev.offsetY;
+  console.log(val);
+  ev.preventDefault();
+}
